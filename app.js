6de6c962e19f61dc14570d35559e2e8676eb9cbd
Daniel Neel <Daniel Neel>
2017-02-09 18:19:23 -0500

    annoying folders

--- app.js
+++ app.js
@@ -3,20 +3,21 @@ var yahooFinance = require('yahoo-finance');
 ///////////////////
 // Get Stock Info
 ///////////////////
-// var currentStockPrice;
-//
-// function getStockInfo(currentStockPrice){
-//   // stock price data from yahoo
-//
-// }
 
+// var name = name.data;
+ /*var symbol = data.symbol;
+ var lastTradeDate = data.lastTradeDate;
+ var currentStockPrice = data.lastTradePriceOnly;
+ var changeInPercent = data.changeInPercent;
+ var dividendYield = data.dividendYield;
+ var dividendPerShare = data.dividendPerShare;*/
 
-yahooFinance.snapshot({
-  symbol: "AAPL",
-  fields: ['s','n','d1','l1', 'p2','y', 'd' ]
-}, function(err, data){
-  if(err)
-    console.log(err);
-  console.log(data);
-  console.log(data.name);
-});
+ yahooFinance.snapshot({
+   symbol: "AAPL",
+   fields: ['s','n','d1','l1', 'p2','y', 'd' ]
+ }, function(err, data){
+    if(err)
+      console.log(err);
+    console.log(data);
+    console.log(data.name);
+ });
