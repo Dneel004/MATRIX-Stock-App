a9ba66dfcda403e27aff9b0c2e10a7e72d3e9c77
Daniel Neel <Daniel Neel>
2017-02-09 18:45:45 -0500

    changed config.yaml

--- app.js
+++ app.js
@@ -4,20 +4,20 @@ var yahooFinance = require('yahoo-finance');
 // Get Stock Info
 ///////////////////
 
-// var name = name.data;
- /*var symbol = data.symbol;
- var lastTradeDate = data.lastTradeDate;
- var currentStockPrice = data.lastTradePriceOnly;
- var changeInPercent = data.changeInPercent;
- var dividendYield = data.dividendYield;
- var dividendPerShare = data.dividendPerShare;*/
-
  yahooFinance.snapshot({
    symbol: "AAPL",
-   fields: ['s','n','d1','l1', 'p2','y', 'd' ]
+   fields: ['s','n','l1', 'p2','y', 'd' ]
  }, function(err, data){
     if(err)
       console.log(err);
     console.log(data);
     console.log(data.name);
+    // Initializing all the variables
+    var name = data.name;
+    var symbol = data.symbol;
+    var currentStockPrice = data.lastTradePriceOnly;
+    var changeInPercent = data.changeInPercent;
+    var dividendYield = data.dividendYield;
+    var dividendPerShare = data.dividendPerShare;
+
  });
