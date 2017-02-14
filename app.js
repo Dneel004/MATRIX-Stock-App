82c8855720e3ecd0b9a7b6c4630a978cb393a20c
Daniel Neel <Daniel Neel>
2017-02-14 17:05:33 -0500

    added percentChange function

--- app.js
+++ app.js
@@ -3,45 +3,72 @@ var yahooFinance = require('yahoo-finance');
 ///////////////////
 // Event Listners
 ///////////////////
-var animation = new EventEmitter();
+//var animation = new EventEmitter();
 //start animation
 
 //continue here.......................................................
 
 ///////////////////
 // Get Stock Info
-///////////////////
+//////////////////
+//Initializing all variables
+var name = "";
+var symbol = "";
+var currentStockPrice = 0.0;
+var changeInPercent = changeInPercent;
+var dividendYield = 0.0;
+var dividendPerShare = 0.0;
 
- yahooFinance.snapshot({
-   symbol: "AAPL",
-   fields: ['s','n','l1', 'p2','y', 'd' ]
- }, function(err, data){
-    if(err)
-      console.log(err);
-    console.log(data);
-    console.log(data.name);
-    // Initializing all the variables
-    var name = data.name;
-    var symbol = data.symbol;
-    var currentStockPrice = data.lastTradePriceOnly;
-    var changeInPercent = data.changeInPercent * 100;
-    var dividendYield = data.dividendYield;
-    var dividendPerShare = data.dividendPerShare;
+function getStockData(name, symbol, currentStockPrice, changeInPercent, dividendYield, dividendPerShare){
+   yahooFinance.snapshot({
+     symbol: "AAPL",
+     fields: ['s','n','l1', 'p2','y', 'd' ]
+   }, function(err, data){
+      if(err)
+        console.log(err);
+      console.log(data);
+      console.log(data.name);
+      // setting all the variables
+      var name = data.name;
+      var symbol = data.symbol;
+      var currentStockPrice = data.lastTradePriceOnly;
+      var changeInPercent = data.changeInPercent;
+      var dividendYield = data.dividendYield;
+      var dividendPerShare = data.dividendPerShare;
 
- });
+   });
+};
+//test getting data
+console.log(getStockData(name, symbol, currentStockPrice, changeInPercent, dividendYield, dividendPerShare));
 
- ///////////////////////////
- // turns on led % change
- ///////////////////////////
-function runPercentChange(percentChange){
+// turns on led % change
+function runPercentChange(){
+  var percentLevel = changeInPercent;
+  var position = 150
+  var increase = Math.PI / 180;
 
+  setInterval(function(){
     //positive percentChange
-    if(changeInPercent > 0.0)
-        matrix.led("green").render();
+    if(changeInPercent > 0.0){
+          var showChange = {
+            arc: 30 + percentLevel,
+            start: position - percentLevel,
+            color: green,
+          };
+          matrix.led(showChange).render();
+    }
     // negative percentChange
-    else if(changeInPercent < 0.0)
-        matrix.led("red").render();
+    else if(changeInPercent < 0.0){
+          var showChange = {
+            arc: 30 + percentLevel,
+            start: position - percentLevel,
+            color: green,
+          };
+          matrix.led(showChange).render();
+    }
     // no percentChange
     else
         matrix.led("yellow").render();
+  },5000);
+
 }
